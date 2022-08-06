a9dba2169a49b4ade365c93bf0f31ae55e46abda
adrish <dev.adrishs@gmail.com>
2022-08-06 16:43:30 +0500

    project setup

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="h-screen w-screen flex justify-center items-center">
+      <h1 className='shadow-md p-2'>React App</h1>
+    </div>
+  );
+}
+
+export default App;
