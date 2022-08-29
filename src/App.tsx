11b1736ee319ae8be603fe808b684e651f754ef8
Adrish <83942036+devAdrish@users.noreply.github.com>
2022-08-29 12:11:39 +0500

    spinner (#6)
    Co-authored-by: aiman tahir <aimantahir1225@gmail.com>

--- src/App.tsx
+++ src/App.tsx
@@ -7,32 +7,32 @@ import SignUp from "components/signup";
 import Dashboard from "components/dashboard";
 import ProtectedRoute from "routes";
 import Layout from "components/layout";
-import Loading from "components/loader"
-
+import Spinner from "components/spinner";
 
 function App() {
   return (
-      <div className="h-screen w-screen">
-        <BrowserRouter>
+    <div className="h-screen w-screen">
+      <BrowserRouter>
         <Layout>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/sign-up" element={<SignUp />} />
-          <Route path="/dashboard" element={
-            <ProtectedRoute>
-              <Dashboard />
-            </ProtectedRoute>
-          }
-          />
-        </Routes>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/sign-up" element={<SignUp />} />
+            <Route
+              path="/dashboard"
+              element={
+                <ProtectedRoute>
+                  <Dashboard />
+                </ProtectedRoute>
+              }
+            />
+          </Routes>
         </Layout>
-      </BrowserRouter >
-      <div className="flex justify-center mt-20">
-      <Loading/>
+      </BrowserRouter>
+      <div className="grid place-items-center h-screen">
+        <Spinner show={true} />
       </div>
-      </div >
-  
+    </div>
   );
 }
 
--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -5,12 +5,12 @@ import { useNavigate } from "react-router-dom";
 const Navbar = () => {
     const navigate = useNavigate();
     return (
-        <div className=" content-end flex justify-end">
+        <div className=" content-end border-b-2 flex justify-end mb-2">
             
-            <button className="bg-green-500 text-white rounded-md px-4 py-2 m-5" onClick={() => { navigate("/sign-up") }}>
+            <button className="bg-green-500 text-white rounded-md px-4 py-2 m-1" onClick={() => { navigate("/sign-up") }}>
                 Sign Up
             </button>
-            <button className="bg-blue-500 text-white rounded-md px-4 py-2 m-5" onClick={() => { navigate("/login") }}>
+            <button className="bg-blue-500 text-white rounded-md px-4 py-2 m-1 mr-2" onClick={() => { navigate("/login") }}>
                 Login
             </button>
         </div>
