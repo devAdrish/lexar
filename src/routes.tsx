a344dba5f6b8e2075e743b97f9f8595ac421d4a1
adrish <dev.adrishs@gmail.com>
2022-10-03 10:45:08 +0500

    fix routing

--- src/routes.tsx
+++ src/routes.tsx
@@ -2,16 +2,24 @@ import React from "react";
 import { Navigate } from "react-router-dom";
 import { useAuth } from "utils/functions";
 
+let navigatedToInstallOnce = false;
+
 const ProtectedRoute = ({ children }: { children: JSX.Element }) => {
   const { isLoggedIn } = useAuth();
-  const isStandalone = window.matchMedia('(display-mode: standalone)').matches
-  if(!isStandalone) return <Navigate to="/home" />
+  const isStandalone = window.matchMedia("(display-mode: standalone)").matches;
+  if (!isStandalone && !navigatedToInstallOnce) {
+    navigatedToInstallOnce = true;
+    return <Navigate to="/home" />;
+  }
   return isLoggedIn ? children : <Navigate to="/login" />;
 };
 const ProtectedLoginSignUp = ({ children }: { children: JSX.Element }) => {
   const { isLoggedIn } = useAuth();
-  const isStandalone = window.matchMedia('(display-mode: standalone)').matches
-  if(!isStandalone) return <Navigate to="/home" />
+  const isStandalone = window.matchMedia("(display-mode: standalone)").matches;
+  if (!isStandalone && !navigatedToInstallOnce) {
+    navigatedToInstallOnce = true;
+    return <Navigate to="/home" />;
+  }
   return !isLoggedIn ? children : <Navigate to="/" />;
 };
 
